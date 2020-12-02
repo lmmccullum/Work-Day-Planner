6d2a11b40a91e26325055b4c9281f848ff3f2337
Latisha McCullum <m.latisha@yahoo.com>
2020-12-02 13:59:34 -0600

    fixed local storage

--- script.js
+++ script.js
@@ -1,58 +1,57 @@
 // Global Variables
 
-var hour = parseInt(moment().format("HH"))
-var now = moment()
-var formattedDate = now.format('dddd MMMM Do YYYY')
-console.log(formattedDate)
+var hour = parseInt(moment().format("HH"));
+var now = moment();
+var formattedDate = now.format("dddd MMMM Do YYYY");
+console.log(formattedDate);
 var array = [];
 
 // Color Coding/OnClick
 
 $(document).ready(function () {
+  $("#currentDay").text(formattedDate);
 
-    $('#currentDay').text(formattedDate)
-
-    for (var i = 1; i < 10; i++) {
-        timeValue = parseInt(moment($('#t' + i).data('hour'), 'HH').format('HH'))
-        //textTime = parseInt(moment($('#time'+i).data('time'),'HH').format('HH'))
-        console.log($('#' + i))
-        if (timeValue < hour) {
-            $("#" + i).addClass('past')
-        } else if (timeValue > hour) {
-            $("#" + i).addClass('future')
-        } else if (timeValue === hour) {
-            $("#" + i).addClass('present')
-        }
+  for (var i = 1; i < 10; i++) {
+    timeValue = parseInt(moment($("#t" + i).data("hour"), "HH").format("HH"));
+    //textTime = parseInt(moment($('#time'+i).data('time'),'HH').format('HH'))
+    console.log($("#" + i));
+    if (timeValue < hour) {
+      $("#" + i).addClass("past");
+    } else if (timeValue > hour) {
+      $("#" + i).addClass("future");
+    } else if (timeValue === hour) {
+      $("#" + i).addClass("present");
     }
+  }
 
-    // Local Storage 
+  // Local Storage
 
-    $(".saveBtn").on("click", function () {
-        var tId;
-        var Text;
+  $(".saveBtn").on("click", function () {
+    var tId;
+    var Text;
 
-        tId = $(this).attr('data-button')
-        Text = $('#' + tId).val()
+    tId = $(this).attr("data-button");
+    Text = $("#" + tId).val();
 
-        array.push({ 'id': tId, 'task': Text });
-        localStorage.setItem("tasks", JSON.stringify(array));
-    })
+    array.push({ id: tId, task: Text });
+    localStorage.setItem("tasks", JSON.stringify(array));
+  });
 
-    // Parsing the JSON string to an object
-    // Get stored tasks from localStorage
+  // Parsing the JSON string to an object
+  // Get stored tasks from localStorage
 
-    function init() {
-        var storedTasks = JSON.parse(localStorage.getItem("tasks"));
+  function init() {
+    var storedTasks = JSON.parse(localStorage.getItem("tasks"));
 
-
-        if (storedTasks !== null) {
-            array = storedTasks;
-        }
-
-        for (var i = 0; i < array.length; i++) {
-            var task = (storedTasks[i].task);
-            $('#' + i).val(task);
-        }
+    if (storedTasks !== null) {
+      array = storedTasks;
     }
-})
-
+console.log(array, "<=====")
+    for (var i = 0; i < array.length; i++) {
+      var task = (storedTasks[i].task);
+      var id = (storedTasks[i].id)
+      $("#" + id).val(task);
+    }
+  }
+  init()
+});
